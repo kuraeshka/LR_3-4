7e2e867f40aeadb90db354e98549da929c67a4dd
kuraeshka <144653643+kuraeshka@users.noreply.github.com>
2023-09-11 09:14:21 +0300

    Add files via upload
    Переделал

---
+++ 25.pas
@@ -0,0 +1,16 @@
+﻿Program zad25;
+var
+  a,b,c,d,e,f:integer;
+  begin 
+    write('Введите сумму покупки ');
+    readln(a,b);
+  write('Введите сумму взноса ');
+    readln(c,d);
+    b:=b+100;
+    a:=a-1;
+    e:=a-c;
+    f:=b-d;
+    if f>100 then e:=e+1;
+    if f>100 then f:=f-100;
+    write('сдача ',e,' руб ' ,f, ' коп ');
+  end.
